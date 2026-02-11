eeaf6ffeee7e24882f26823c686bfbe1b6b7d8e5
ap3305 <ap3305@bath.ac.uk>
2026-02-11 20:41:56 +0000

    chore: Space theorem docs

--- Game/Levels/Inner_Product.lean
+++ Game/Levels/Inner_Product.lean
@@ -1,5 +1,7 @@
 import Game.Levels.Inner_Product.L01_Add_Inner
 import Game.Levels.Inner_Product.L02_Inner_Zero
+import Game.Levels.Inner_Product.L03_Inner_Neg
+import Game.Levels.Inner_Product.L04_Inner_Self
 
 World "Inner_Product"
 Title "Inner Product World"
--- Game/Theorems/Space/TheoremDocs.lean
+++ Game/Theorems/Space/TheoremDocs.lean
@@ -82,57 +82,57 @@ TacticDoc vector_simp
 
 
 /--
-some description
+`add_zero a` is a proof that `a + 0 = 0`.
 -/
 TheoremDoc add_zero as "add_zero" in "ℝ"
 
 /--
-some description
+`zero_add a` is a proof that `0 + a = 0`.
 -/
 TheoremDoc zero_add as "zero_add" in "ℝ"
 
 /--
-some description
+`add_neg_cancel a` is a proof that `a + -a = 0`.
 -/
 TheoremDoc add_neg_cancel as "add_neg_cancel" in "ℝ"
 
 /--
-some description
+`neg_add_cancel a` is a proof that `-a + a = 0`.
 -/
 TheoremDoc neg_add_cancel as "neg_add_cancel" in "ℝ"
 
 /--
-some description
+`add_comm a b` is a proof that `a + b = b + a`.
 -/
 TheoremDoc add_comm as "add_comm" in "ℝ"
 
 /--
-some description
+`add_assoc a b c` is a proof that `(a + b) + c = a + (b + c)` where `(a + b) + c` is also written as `a + b + c` in Lean.
 -/
 TheoremDoc add_assoc as "add_assoc" in "ℝ"
 
 /--
-some description
+`mul_add a b c` is a proof that `a * (b + c) = a * b + a * c`.
 -/
 TheoremDoc mul_add as "mul_add" in "ℝ"
 
 /--
-some description
+`mul_add a b c` is a proof that `(a + b) * c = a * c + b * c`.
 -/
 TheoremDoc add_mul as "add_mul" in "ℝ"
 
 /--
-some description
+`one_mul a` is a proof that `1 * a = a`.
 -/
 TheoremDoc one_mul as "one_mul" in "ℝ"
 
 /--
-some description
+`one_mul a` is a proof that `a * 1 = a`.
 -/
 TheoremDoc mul_one as "mul_one" in "ℝ"
 
 /--
-some description
+`mul_assoc` is a proof that `(a * b) * c = a * (b * c)` where `(a * b) * c` is also written as `a * b * c` in Lean.
 -/
 TheoremDoc mul_assoc as "mul_assoc" in "ℝ"
 
@@ -140,50 +140,54 @@ TheoremDoc mul_assoc as "mul_assoc" in "ℝ"
 
 
 /--
-some description
+`vector_add_zero v` is a proof that `v + 0 = 0` where `v` is a 3 dimensional vector of type `MySpace` or a 4 dimensional vector of type `MySpacetime`.
 -/
 TheoremDoc MySpace.vector_add_zero as "vector_add_zero" in "Vectors"
 
 /--
-some description
+`vector_add_inv v` is a proof that `v + -v = 0` where `v` is a 3 dimensional vector of type `MySpace` or a 4 dimensional vector of type `MySpacetime`.
 -/
 TheoremDoc MySpace.vector_add_inv as "vector_add_inv" in "Vectors"
 
 /--
-some description
+`vector_add_comm u v` is a proof that `u + v = v + u` where `u v w` are 3 dimensional vectors of type `MySpace` or 4 dimensional vectors of type `MySpacetime`.
 -/
 TheoremDoc MySpace.vector_add_comm as "vector_add_comm" in "Vectors"
 
 /--
-some description
+`vector_add_assoc u v w` is a proof that `(u + v) + w = u + (v + w)` where `(u + v) + w` is also written as `u + v + w` in Lean.
+`u v w` are 3 dimensional vectors of type `MySpace` or 4 dimensional vectors of type `MySpacetime`.
 -/
 TheoremDoc MySpace.vector_add_assoc as "vector_add_assoc" in "Vectors"
 
 /--
-some description
+`vector_smul_one u` is a proof that `1 • u = u` where `u` is a 3 dimensional vector of type `MySpace` or a 4 dimensional vector of type `MySpacetime`.
 -/
 TheoremDoc MySpace.vector_smul_one as "vector_smul_one" in "Vectors"
 
 /--
-some description
+`vector_smul_add u v a` is a proof that `a • (u + v) = a • u + a • v` where `u v` are 3 dimensional vectors of type `MySpace` or 4 dimensional vectors of type `MySpacetime`, and `a` is a real number.
 -/
 TheoremDoc MySpace.vector_smul_add as "vector_smul_add" in "Vectors"
 
 /--
-some description
+`vector_add_smul u a b` is a proof that `(a + b) • u = a • u + b • u` where `u` is a 3 dimensional vector of type `MySpace` or a 4 dimensional vector of type `MySpacetime`, and `a b` are real numbers.
 -/
 TheoremDoc MySpace.vector_add_smul as "vector_add_smul" in "Vectors"
 
 /--
-some description
+`vector_smul_assoc u a b` is a proof that `a • (b • u) = (a * b) • u`.
+`u` is a 3 dimensional vector of type `MySpace` or a 4 dimensional vector of type `MySpacetime`, and `a b` are real numbers.
 -/
-TheoremDoc MySpace.vector_smul_assoc as "vector_add_assoc" in "Vectors"
+TheoremDoc MySpace.vector_smul_assoc as "vector_smul_assoc" in "Vectors"
 
 
 
 
 
 /--
-some description
+`MySpace` is a type representing a 3 dimension vector.
+- `v : MySpace` creates a vector `v`.
+- `v := { x := 1, y := 2, z := 3 }` assigns values to `v` which can be accessed with `v.x`, `v.y`, and `v.z`.
 -/
 DefinitionDoc MySpace as "MySpace"
