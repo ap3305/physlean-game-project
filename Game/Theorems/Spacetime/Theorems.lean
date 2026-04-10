95f7364713215ae079e913f5f1ec623a4ca13e38
ap3305 <ap3305@bath.ac.uk>
2026-04-10 14:14:47 +0100

    refactor!: Ex[erimenting with changing MySpacetime to a structure of MySpace and MyTime

--- Game/Theorems/Spacetime/Theorems.lean
+++ Game/Theorems/Spacetime/Theorems.lean
@@ -1,49 +1,29 @@
 import GameServer
 import Game.Metadata
+import Game.Theorems.Space.Theorems
+import Game.Theorems.Time.Theorems
 
 @[ext]
 structure MySpacetime where
-  t : ℝ
-  x : ℝ
-  y : ℝ
-  z : ℝ
+  t : MyTime
+  s : MySpace
 
 namespace MySpacetime
 
 instance : Add MySpacetime where
-  add u v := { t := u.t + v.t, x := u.x + v.x, y := u.y + v.y, z := u.z + v.z }
+  add u v := { t := u.t + v.t, s := u.s + v.s }
 
 instance : Sub MySpacetime where
-  sub u v := { t := u.t - v.t, x := u.x - v.x, y := u.y - v.y, z := u.z - v.z }
+  sub u v := { t := u.t - v.t, s := u.s - v.s }
 
 instance : Neg MySpacetime where
-  neg u := { t := -u.t, x := -u.x, y := -u.y, z := -u.z }
+  neg u := { t := -u.t, s := -u.s }
 
 instance : SMul ℝ MySpacetime where
-  smul a u := { t := a * u.t, x := a * u.x, y := a * u.y, z := a * u.z }
+  smul a u := { t := a • u.t, s := a • u.s }
 
 instance : Zero MySpacetime where
-  zero := { t := 0, x := 0, y := 0, z := 0 }
-
-@[simp] lemma add_t (u v : MySpacetime) : (u + v).t = u.t + v.t := rfl
-@[simp] lemma add_x (u v : MySpacetime) : (u + v).x = u.x + v.x := rfl
-@[simp] lemma add_y (u v : MySpacetime) : (u + v).y = u.y + v.y := rfl
-@[simp] lemma add_z (u v : MySpacetime) : (u + v).z = u.z + v.z := rfl
-
-@[simp] lemma zero_t : (0 : MySpacetime).t = 0 := rfl
-@[simp] lemma zero_x : (0 : MySpacetime).x = 0 := rfl
-@[simp] lemma zero_y : (0 : MySpacetime).y = 0 := rfl
-@[simp] lemma zero_z : (0 : MySpacetime).z = 0 := rfl
-
-@[simp] lemma neg_t (u : MySpacetime) : (-u).t = -u.t := rfl
-@[simp] lemma neg_x (u : MySpacetime) : (-u).x = -u.x := rfl
-@[simp] lemma neg_y (u : MySpacetime) : (-u).y = -u.y := rfl
-@[simp] lemma neg_z (u : MySpacetime) : (-u).z = -u.z := rfl
-
-@[simp] lemma smul_t (u : MySpacetime) (a : ℝ) : (a • u).t = a * u.t := rfl
-@[simp] lemma smul_x (u : MySpacetime) (a : ℝ) : (a • u).x = a * u.x := rfl
-@[simp] lemma smul_y (u : MySpacetime) (a : ℝ) : (a • u).y = a * u.y := rfl
-@[simp] lemma smul_z (u : MySpacetime) (a : ℝ) : (a • u).z = a * u.z := rfl
+  zero := { t := 0, s := 0 }
 
 
 def my_minkowski : Matrix (Fin 4) (Fin 4) ℝ :=
@@ -51,7 +31,7 @@ def my_minkowski : Matrix (Fin 4) (Fin 4) ℝ :=
 
 notation "η" => my_minkowski
 
-def MyBilinear (u v : MySpacetime) : ℝ := u.t * v.t - u.x * v.x - u.y * v.y - u.z * v.z
+def MyBilinear (u v : MySpacetime) : ℝ := u.t.val * v.t.val - u.s.x * v.s.x - u.s.y * v.s.y - u.s.z * v.s.z
 
 notation "≪" u ", " v "≫" => MyBilinear u v
 
--- Game/Theorems/Time/Theorems.lean
+++ Game/Theorems/Time/Theorems.lean
@@ -5,8 +5,35 @@ import Game.Metadata
 structure MyTime where
   val : ℝ
 
+namespace MyTime
+
+instance : Add MyTime where
+  add u v := { val := u.val + v.val }
+
+instance : Sub MyTime where
+  sub u v := { val := u.val - v.val }
+
+instance : Neg MyTime where
+  neg u := { val := -u.val }
+
+instance : SMul ℝ MyTime where
+  smul a u := { val := a * u.val }
+
+instance : Zero MyTime where
+  zero := { val := 0 }
+
+@[simp] lemma add_t (u v : MyTime) : (u + v).val = u.val + v.val := rfl
+
+@[simp] lemma zero_t : (0 : MyTime).val = 0 := rfl
+
+@[simp] lemma neg_t (u : MyTime) : (-u).val = -u.val := rfl
+
+@[simp] lemma smul_t (u : MyTime) (a : ℝ) : (a • u).val = a * u.val := rfl
+
 def is_past (t: MyTime) : Prop := t.val < 0
 
 def is_present (t: MyTime) : Prop := t.val == 0
 
 def is_future (t: MyTime) : Prop := t.val > 0
+
+end MyTime
