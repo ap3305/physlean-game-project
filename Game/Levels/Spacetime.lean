d5f0bafaf8a6915355aa01c86ba0db02e6cc2ef1
ap3305 <ap3305@bath.ac.uk>
2026-04-12 16:26:34 +0100

    fix: Add the new levels to worlds

--- Game/Levels/Spacetime.lean
+++ Game/Levels/Spacetime.lean
@@ -1,5 +1,7 @@
 import Game.Levels.Spacetime.L01_Bilinear_Applicable
 import Game.Levels.Spacetime.L02_Unit_c
+import Game.Levels.Spacetime.L03_Exclusivity
+import Game.Levels.Spacetime.L04_Scalar_Lightlike
 
 World "Spacetime"
 Title "Minkowski Spacetime"
--- Game/Levels/Time.lean
+++ Game/Levels/Time.lean
@@ -1,5 +1,6 @@
 import Game.Levels.Time.L01_Time
 import Game.Levels.Time.L02_Exclusivity
+import Game.Levels.Time.L03_Scalar_Present
 
 World "Time"
 Title "Time World"
